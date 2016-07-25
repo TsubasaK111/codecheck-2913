6159856fe73d8fe26932e1d6809169e01be2fe39
TsubasaK111 <Tsubasa.K111@gmail.com>
2016-07-25 17:05:08 +0900

    feat: complete tests and add overflow prevention

--- app/app.py
+++ app/app.py
@@ -1,52 +1,41 @@
-import pdb, sys
+import pdb, logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename='logs.log',
+                    filemode='w')
 
 def main(arguments, options):
     for argument in arguments:
-        input_number = int(argument)
-
+        recursion_times = int(argument)
+        # initial input must be set manually to '1'
         number = "1"
-        number_block = ""
-        for h in xrange(1,input_number):
-            for i,v in enumerate(number):
-                number_block += v
-                if ((i+1) >= len(number)) or not number[i] == number[i+1]:
-                    block_expression = str(len(number_block)) + v
-                    output_number += block_expression
-                else:
-                    output_number = "huh?" + i + v
-            number = output_number
-
-                #     number_block += v
-                # elif len(number_block) > 0:
-                #     block_expression = str(len(number_block)) + v
-                #     number += block_expression
-                # else: number = "11"
-                # number += block_expression
-
-
-            # for digit in enumerate(argument):
-            #     numbers.append(digit)
-            # previous_digit = 0
-            # same_digits = []
-            # for digit in numbers:
-            #     string = ""
-            #     for thing in digit:
-            #         string += str(thing)
-            #     print string
-            #
-            #     if int(digit) == int(previous_digit):
-            #         same_digits.append(digit)
-            #     else:
-            #         same_digits_count = len(same_digits)
-            #         if same_digits_count>0:
-            #             output_numbers.append(same_digits_count)
-            #             output_numbers.append(same_digits[0])
-            #         same_digits = []
-            #     previous_digit = digit
+        for i in xrange(1,recursion_times):
+            number = say_number(str(number))
         print number
 
-# def main(arguments, options):
-#     numbers = ""
-#     for argument in arguments:
-#         numbers += argument
-#     print numbers
+def say_number(number):
+    """Takes 'number' input and reads it out in the look-and-say method."""
+    number_block = ""
+    output_number = ""
+
+    for i,v in enumerate(number):
+        number_block += v
+        if ((i+1) >= len(number)) or not (number[i] == number[i+1]):
+            #example: 111 will be expressed as '31', in other words 'three ones'
+            block_expression = str(len(number_block)) + v
+            output_number += block_expression
+            logging.debug( """ End of Block. (number={n}, number_block={b},block_expression={e})
+                          """.format( n=number,
+                                      b=number_block,
+                                      e=block_expression ))
+            number_block = ""
+        # Prevent overflow by pruning to 500 digits and breaking.
+        if len(output_number)>500:
+            logging.warning(""" Reached 500 digit limit. Length of output is: {l}
+                          """.format( l= len(output_number )))
+            output_number = output_number[0:500]
+            break
+
+    logging.debug( """ End of say_number. (output_number={o}, number={n})
+                  """.format( o=output_number, n=number ))
+    return output_number
